3fddb29dd9dbb8c4a5c4ae9fa97b8fa7b22d7436
Jonus <joclapshaw@gmail.com>
2026-05-12 10:42:41 -0700

    Simple loading list

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Card from './components/Card'
@@ -6,8 +5,6 @@ import Card from './components/Card'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Header />
