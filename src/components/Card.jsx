885c83e385f0206465362396be08b2a352fd97c7
kevinrsandoval <174657099+KevinSandoval12@users.noreply.github.com>
2026-05-12 10:53:46 -0700

    push

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,35 +1,35 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 export default function Card() {
-    const[ pokemon, setPokemon ] = useState([])
-    const[ loading, setLoading ] = useState(true)
+  const [pokemon, setPokemon] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        fetch('https://pokeapi.co/api/v2/pokemon/')
-        .then((response) => response.json())
-        .then((data) => {
-            setPokemon(data.results)
-            setLoading(false);
-        })
-        .catch((error) => {
-            console.log("no pokemon found", error)
-            setLoading(false);
-        });
-    }, []);
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/")
+      .then((response) => response.json())
+      .then((data) => {
+        setPokemon(data.results);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log("no pokemon found", error);
+        setLoading(false);
+      });
+  }, []);
 
-    if(loading){
-        return <h1> ... loading </h1>
-    }
+  if (loading) {
+    return <h1> ... loading </h1>;
+  }
 
-    return (
-        <div>
-            <h1> Pokemon </h1>
+  return (
+    <div>
+      <h1> Pokemon </h1>
 
-            {pokemon.map((poke) => (
-                <div key={poke.name}>
-                <h2>{poke.name}</h2>
-                </div>
-            ))}
+      {pokemon.map((poke) => (
+        <div key={poke.name}>
+          <h2>{poke.name}</h2>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
